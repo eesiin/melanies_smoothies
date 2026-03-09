2f255d2af96b514eae9abaf19f32608a9c8ff1cc
eesiin <eesiin@gmail.com>
2026-03-09 10:17:57 +0100

    Update streamlite_app.py
    disable session

--- streamlite_app.py
+++ streamlite_app.py
@@ -17,7 +17,7 @@ title = st.text_input("Name on Smothie", "")
 st.write("The name on your Smoothie will be ", title)
 
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
