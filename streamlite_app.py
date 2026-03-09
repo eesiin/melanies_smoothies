1be212e019757949f988d92c58e429d7b3799d79
eesiin <eesiin@gmail.com>
2026-03-09 10:44:11 +0100

    Update streamlite_app.py
    make json in dataframe

--- streamlite_app.py
+++ streamlite_app.py
@@ -5,6 +5,7 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())  # .json() to display readable response
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
